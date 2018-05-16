13aa6e1a4dea5facddd4e01b014d86473a7bfe9e
Finn <tupfi2@gmx.de>
2018-05-16 16:46:27 +0200

    Added the receive example

---
+++ experiments/receive.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from smbus import SMBus
+import sys
+
+# Init sensor port 2...
+bus = SMBus(4)
+
+# Read all bytes from the Arduino... (All bytes:    A = accel data
+#                                                                                  M = magnetometer data
+#                                                                                  G = gyro data
+#                                                                                  T = temperature
+data = bus.read_i2c_block_data(0x04, ord(sys.argv[1]))
+
+# Connect the bytes to a string...
+msg = ""
+for byte in data:
+    if byte != 255:
+        # Convert the byte to a char and add this to the string...
+        msg += str(chr(byte))
+        
+# Print the message...
+print(msg)
