7301381e774f1e9ce5f1f8c275db802263b0170d
Finn <tupfi2@gmx.de>
2018-05-16 16:00:07 +0200

    Add the experiment 'motor accuracy'

--- experiments/motorAccuracy.py
+++ experiments/motorAccuracy.py
@@ -4,7 +4,8 @@
 # For more detailed information you can read the wiki article 'Improve the accuracy of the robot' on the page 'experiments'
 
 import ev3dev.ev3 as ev3
-import time
+from time import sleep
+import sys
 
 # Init the motors...
 motorR = ev3.LargeMotor("outC")
@@ -19,8 +20,8 @@ def timeAndSpeed(time, speedR, speedL):
     motorL.run_timed(time_sp = time, speed_sp = speedL)
     
     # Wait until the motors stop...
-    while motorR.isRunning() or motorL.isRunning():
-        time.sleep(0.1)
+    while motorR.is_running or motorL.is_running:
+        sleep(0.1)
 
 def tachoPosition(speed, unitsR, unitsL):
     """Driving forward to the given position"""
@@ -34,12 +35,11 @@ def tachoPosition(speed, unitsR, unitsL):
     motorL.run_to_abs_pos(position_sp=absPositionL, speed_sp=speed)
     
     # Wait until the motors stop...
-    while motorR.isRunning() or motorL.isRunning():
-        time.sleep(0.1)
+    while motorR.is_running or motorL.is_running:
+        sleep(0.1)
 
 # Choose which example should be execute...
-timeAndSpeed(100, 500, 500)
-time.sleep(2)
-tachoPosition(100, 1000, 1000)
+print(sys.argv)
+tachoPosition(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
 
 print("Ready!")
